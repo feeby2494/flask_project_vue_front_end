cb2969fed7eebdfe2c73ce9567e0ff3b1d56a1df
Jamie Lynn <toby2494@hotmail.com>
2019-12-27 12:05:46 -0600

    added web service web forms

--- src/views/DeviceRepair.vue
+++ src/views/DeviceRepair.vue
@@ -4,6 +4,7 @@
 
     </div>
     <div class="wrapper mt-3">
+      <!-- Repair form -->
       <b-card no-body class="overflow-hidden" style="max-width:800px;">
         <b-row no-gutters>
           <b-col md="6">
@@ -104,7 +105,7 @@
                   <div class="" v-show="step === 2">
 
                     <b-button variant="info" class="mr-2" @click.prevent="prev()">Previous</b-button>
-                    <b-button variant="info" type="submit" value="Save">Finished</b-button>
+                    <b-button variant="info" @click.prevent="submitRepair()" value="Save">Finished</b-button>
                   </div>
                 </b-form>
               </b-card-text>
@@ -119,77 +120,63 @@
             <b-card-img :src="require(`../assets/imagesForVueClient/web_services.png`)" class="rounded-0"></b-card-img>
           </b-col>
           <b-col md="6">
-            <b-card-body title="website Services">
+            <b-card-body title="Website Services">
               <b-card-text>
                 <b-form class="" action="index.html" method="post">
-                  <div class="" v-show="step === 0">
+                  <div class="" v-show="appStep === 0">
                     <b-form-group
-                      id="deviceType"
-                      label="Select your Device Type:"
-                      label-for="deviceTypeInput-1"
+                      id="appType"
+                      label="Select your App Type:"
+                      label-for="appTypeInput-1"
                     >
-                      <b-form-select id="deviceTypeInput-1" name="deviceType" v-model="repair.deviceType">
-                        <option value="notebook">Notebook</option>
-                        <option value="smartphone">Smartphone</option>
-                        <option value="tablet">Tablet</option>
-                        <option value="desktop">Desktop computer</option>
-                        <option value="chromebook">Chromebook</option>
+                      <b-form-select id="deviceTypeInput-1" name="appType" v-model="app.appType">
+                        <option value="threePageWebsite">Three page website Static site</option>
+                        <option value="webform">Build a Web form frontend</option>
+                        <option value="api">Build API using flask Python</option>
+                        <option value="flaskApp">Build dynamic Flask App</option>
+                        <option value="customVueApp">Build a custom app using Vue.js</option>
+                        <option value="flaskAppWithVue">Build a Flask backend with a Vue.js frontend</option>
+                        <option value="consultation">Consultation and Exiting app Troublshooting</option>
+                        <option value="other">Other requests</option>
                       </b-form-select>
                     </b-form-group>
-                    <div class="" v-show="repair.deviceType === 'notebook'">
-                      <p>notebook</p>
-                      <b-form-group
-                        id="notebookBrand"
-                        label="Brand:"
-                        label-for="notebookBrandInput-1"
-                      >
-                        <b-form-select id="notebookBrandInput-1" name="notebookBrand" v-model="repair.brand">
-                          <option value="Samsung">Samsung</option>
-                          <option value="LG">LG</option>
-                          <option value="Purism">Purism</option>
-                          <option value="Dell">Dell</option>
-                          <option value="HP">HP</option>
-                          <option value="Xiaomi">Xiaomi</option>
-                          <option value="Asus">Asus</option>
-                          <option value="Acer">Acer</option>
-                          <option value="MSI">Msi</option>
-                          <option value="Lenovo">Lenovo</option>
-                          <option value="Microsoft">Microsoft</option>
-                          <option value="Apple">Apple</option>
-                        </b-form-select>
-                      </b-form-group>
-                    </div>
-                    <div class="" v-show="repair.deviceType === 'smartphone'">
-                      <p>smartphone</p>
-                      <b-form-group
-                        id="smartphoneBrand"
-                        label="Brand:"
-                        label-for="smartphoneBrandInput-1"
-                      >
-                        <b-form-select  id="smartphoneBrandInput-1" name="smartphoneBrand" v-model="repair.brand">
-                          <option v-for=" brand in phones " v-bind:value="brand.name">{{ brand.name }}</option>
-                        </b-form-select>
-                      </b-form-group>
 
-                    </div>
-                    <div class="" v-show="repair.deviceType === 'desktop'">
-                      <p>desktop</p>
-                    </div>
-                    <div class="" v-show="repair.deviceType === 'chromebook'">
-                      <p>chromebook</p>
-                    </div>
-                    <b-button variant="info" @click.prevent="next()">Next</b-button>
+
+                    <b-button variant="info" @click.prevent="nextApp()">Next</b-button>
                   </div>
-                  <div class="" v-show="step === 1">
+                  <div class="" v-show="appStep === 1">
+                    <b-form-group
+                      id="customerName"
+                      label="Full Name:"
+                      label-for="nameInput-1"
+                    >
+                      <b-form-input id="nameInput-1" name="name" v-model="app.customerName"></b-form-input>
+                    </b-form-group>
+                    <b-form-group
+                      v-show="app.customerName"
+                      id="email"
+                      label="Email:"
+                      label-for="emailInput-1"
+                    >
+                      <b-form-input id="emailInput-1" name="email" v-model="app.customerEmail"></b-form-input>
+                    </b-form-group>
 
 
-                    <b-button variant="info" class="mr-2" @click.prevent="prev()">Previous</b-button>
-                    <b-button variant="info" @click.prevent="next()">Next</b-button>
+                    <b-button variant="info" class="mr-2" @click.prevent="prevApp()">Previous</b-button>
+                    <b-button  :variant="app.customerName && app.customerEmail? 'info' : 'danger' " @click.prevent="nextApp()">Next</b-button>
+                    <b-alert v-model="showAlert" v-show="!app.customerName || !app.customerEmail" variant="danger" dismissible>
+                      <h3 :style="disclaimer">Disclaimer: While I'm happy to take on new projects, please relise that I sometimes
+                        take on too much at one time and I'm not a senior developer. There are still things I don't
+                        know concerning Javascript and Python. Please be paitence with me and be understanding while I
+                        continue to learn more about web development.
+                      </h3>
+                    </b-alert>
+                    <b-button pill variant="outline-danger" v-show="!app.customerName || !app.customerEmail"> Please enter Email and full name</b-button>
                   </div>
-                  <div class="" v-show="step === 2">
+                  <div class="" v-show="appStep === 2">
 
-                    <b-button variant="info" class="mr-2" @click.prevent="prev()">Previous</b-button>
-                    <b-button variant="info" type="submit" value="Save">Finished</b-button>
+                    <b-button variant="info" class="mr-2" @click.prevent="prevApp()">Previous</b-button>
+                    <b-button variant="info" @click.prevent="submitApp()" value="Save">Finished</b-button>
                   </div>
                 </b-form>
               </b-card-text>
@@ -207,7 +194,7 @@
             <b-card-body title="Automotive Services">
               <b-card-text>
                 <b-form class="" action="index.html" method="post">
-                  <div class="" v-show="step === 0">
+                  <div class="" v-show="autoStep === 0">
                     <b-form-group
                       id="deviceType"
                       label="Select your Device Type:"
@@ -251,9 +238,9 @@
                     <div class="" v-show="repair.deviceType === 'chromebook'">
                       <p>chromebook</p>
                     </div>
-                    <b-button variant="info" @click.prevent="next()">Next</b-button>
+                    <b-button variant="info" @click.prevent="nextAuto()">Next</b-button>
                   </div>
-                  <div class="" v-show="step === 1">
+                  <div class="" v-show="autoStep === 1">
                     <p>{{ repair.brand }}</p>
                     <p>{{ repair.model }}</p>
                     <p>{{ repair.deviceType }}</p>
@@ -266,13 +253,13 @@
 
                     </div>
 
-                    <b-button variant="info" class="mr-2" @click.prevent="prev()">Previous</b-button>
-                    <b-button variant="info" @click.prevent="next()">Next</b-button>
+                    <b-button variant="info" class="mr-2" @click.prevent="prevAuto()">Previous</b-button>
+                    <b-button variant="info" @click.prevent="nextAuto()">Next</b-button>
                   </div>
-                  <div class="" v-show="step === 2">
+                  <div class="" v-show="autoStep === 2">
 
-                    <b-button variant="info" class="mr-2" @click.prevent="prev()">Previous</b-button>
-                    <b-button variant="info" type="submit" value="Save">Finished</b-button>
+                    <b-button variant="info" class="mr-2" @click.prevent="prevAuto()">Previous</b-button>
+                    <b-button variant="info" @click.prevent="submitAuto()" value="Save">Finished</b-button>
                   </div>
                 </b-form>
               </b-card-text>
@@ -290,6 +277,10 @@ export default {
   data() {
     return {
       step: 0,
+      appStep: 0,
+      autoStep: 0,
+      size: 8,
+      showAlert: true,
       repair: {
         deviceType: '',
         brand: null,
@@ -298,6 +289,11 @@ export default {
         edge: false,
         plus: false,
         serial: '',
+      },
+      app: {
+        appType: '',
+        customerName: null,
+        customerEmail: null
       },
       phones: {
         "Samsung": {
@@ -418,6 +414,51 @@ export default {
         "BlackBerry": {name: "BlackBerry"},
         "Microsoft": {name: "Microsoft"},
         "Apple": {name: "Apple"},
+      },
+      notebooks: {
+        "Samsung": {
+          name: "Samsung",
+          models: {
+            "s3": {
+              name: "s3",
+              versions: {"T-Mobile": {name: 'T-Mobile'}},
+              carriers: {},
+              possibleIssues: {}
+            }
+          }
+        },
+        "LG": {name: "LG"},
+        "Pantech": {name: "Pantech"},
+        "Purism": {name: "Purism"},
+        "Nokia": {name: "Nokia"},
+        "Palm": {name: "Palm"},
+        "Google": {name: "Google"},
+        "HTC": {name: "HTC"},
+        "Vivo": {name: "Vivo"},
+        "Oppo": {name: "Oppo"},
+        "Honor": {name: "Honor"},
+        "Huawei": {name: "Huawei"},
+        "Xiaomi": {name: "Xiaomi"},
+        "Meizu": {name: "Meizu"},
+        "OnePlus": {name: "OnePlus"},
+        "Lenovo": {name: "Lenovo"},
+        "Qiku360": {name: "Qiku360"},
+        "Smartisan": {name: "Smartisan"},
+        "Asus": {name: "Asus"},
+        "ZTE": {name: "ZTE"},
+        "Coolpad": {name: "Coolpad"},
+        "Sony": {name: "Sony"},
+        "Sharp": {name: "Sharp"},
+        "Fujitsu": {name: "Fujitsu"},
+        "Toshiba": {name: "Toshiba"},
+        "Panasonic": {name: "Panasonic"},
+        "NEC": {name: "NEC"},
+        "Kyocera": {name: "Kyocera"},
+        "Freetel": {name: "Freetel"},
+        "Motorola": {name: "Motorola"},
+        "BlackBerry": {name: "BlackBerry"},
+        "Microsoft": {name: "Microsoft"},
+        "Apple": {name: "Apple"},
       }
     }
   },
@@ -428,8 +469,36 @@ export default {
     next() {
       this.step++;
     },
+    prevApp() {
+      this.appStep--;
+    },
+    nextApp() {
+      this.appStep++;
+    },
+    prevAuto() {
+      this.autoStep--;
+    },
+    nextAuto() {
+      this.autoStep++;
+    },
     onSubmit(event) {
 
+    },
+    submitAuto() {
+
+    },
+    submitRepair(){
+
+    },
+    submitApp(){
+
+    }
+  },
+  computed: {
+    disclaimer: function() {
+      return {
+        fontSize: this.size + 'px'
+      };
     }
   }
 }
